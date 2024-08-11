5ca0276a7ac4a26ee21422474cad8255a1e079ca
Rue-ops6 <rowanmahrous01@gmail.com>
2024-08-11 17:13:45 +0200

    fashion start-up

---
+++ database/factories/ProdFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProdFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'prodTitle' => fake()->randomElement(['dresses', 'beverage', 'pots']),
+            'price' => fake()->randomFloat(2),
+            'description' => fake()->text(),
+            'pub' => fake()->numberBetween(0, 1),
+            'image' => fake()->image(public_path('assets/images/product')),        
+        ];
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdController;
+
+
+
+Route::get('/', function () {   #homepage
+    return view('welcome');
+});
+
+
+#<!------Fashion-------¡> 
+Route::group([
+    'prefix' => 'fashion',  #for the uri
+    'controller' => ProdController::class,  #then we'll del the [] from the pages that open in browser
+    'as' => 'prod-'  #for the name
+], function () {
+
+    Route::get('home', 'LatestProds')->name('index');
+    Route::get('add', 'create')->name('add');
+    Route::post('', 'store')->name('store');
+});
+
+
+#<!------upload-------¡> 
+//Route::get('uploadFile', [ExampleController::class,'uploadFile']);
+//Route::post('assets', [ExampleController::class,'uploadimg'])->name('uploadimg');
